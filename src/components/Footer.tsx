2f354302ebe13cb50e83735d2345f7daa1a357ff
itdevwu <opensource@itdevwu.com>
2023-11-13 20:33:16 +0800

    ✨ Add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,19 @@
+import { Prompt } from 'next/font/google'
+import Link from 'next/link';
+
+const Prompt_Footer = Prompt({ subsets: ['latin'], weight: "300" });
+
+const Footer = (props: {
+    user: string,
+    userFooter: string
+}) => {
+    return (
+        <footer
+            className={`${Prompt_Footer.className} text-zinc-800 h-1/8 w-full text-center align-text-bottom bottom-0 pb-0 pt-10 text-xs mb-6`}
+        >
+            {props.userFooter}<br />© {new Date().getFullYear()} <Link href="/">{props.user}</Link>.
+        </footer>
+    )
+}
+
+export default Footer;
