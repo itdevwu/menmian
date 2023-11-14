a4d80f2d0410f4f23ecc471945f66949286bda14
itdevwu <opensource@itdevwu.com>
2023-11-14 12:05:52 +0800

    🐛 Fix animation reload bug when link was clicked

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,6 @@
 import { Prompt } from 'next/font/google'
 import Link from 'next/link';
+import router from 'next/router';
 
 const Prompt_Footer = Prompt({ subsets: ['latin'], weight: "300" });
 
@@ -11,7 +12,9 @@ const Footer = (props: {
         <footer
             className={`${Prompt_Footer.className} text-zinc-800 h-1/8 w-full text-center align-text-bottom bottom-0 pb-0 pt-10 text-xs mb-6`}
         >
-            {props.userFooter}<br />© {new Date().getFullYear()} <Link href="/">{props.user}</Link>.
+            {props.userFooter}<br />© {new Date().getFullYear()} <Link href="/" onClick={() => {
+                router.reload();
+            }}>{props.user}</Link>.
         </footer>
     )
 }
